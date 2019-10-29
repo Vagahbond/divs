87ea0ee84fabf002aa50d636d8b1750dbf459d56
Vagahbond <56230065+Vagahbond@users.noreply.github.com>
2019-10-29 16:16:44 +0100

    Monde de merde

--- src/com/company/Fraction.java
+++ src/com/company/Fraction.java
@@ -1,10 +1,42 @@
 package com.company;
 
 public class Fraction {
-    int denominateur;
-    int numerateur;
+    private int denominateur;
+    private int numerateur;
     public  Fraction(int aDen, int aNum)
     {
-
+        this.denominateur = aDen;
+        this.numerateur = aNum;
     }
+
+    public Fraction addFraction(Fraction aFrac)
+    {
+        if (this.denominateur == aFrac.denominateur)
+        {
+            this.numerateur+= aFrac.numerateur;
+        }
+        else
+        {
+            this.numerateur = this.numerateur*aFrac.denominateur + aFrac.numerateur*this.denominateur;
+            this.denominateur = this.denominateur*aFrac.denominateur;
+        }
+
+        return this;
+    }
+
+    public Fraction productFraction(Fraction aFrac)
+    {
+        if (this.denominateur == aFrac.denominateur)
+        {
+            this.numerateur+= aFrac.numerateur;
+        }
+        else
+        {
+            this.numerateur = this.numerateur*aFrac.denominateur + aFrac.numerateur*this.denominateur;
+            this.denominateur = this.denominateur*aFrac.denominateur;
+        }
+
+        return this;
+    }
+
 }
