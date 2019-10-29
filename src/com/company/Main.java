98cc6c631ee760c23f213d496eb699c5ba4c1efa
Vagahbond <56230065+Vagahbond@users.noreply.github.com>
2019-10-29 16:02:11 +0100

    mes COUILLES

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,15 @@
 package com.company;
 
+import java.util.Scanner;
+
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Scanner scanner = new Scanner(System.in);
 
-        
+        String gender = scanner.next();
+
     }
 }
